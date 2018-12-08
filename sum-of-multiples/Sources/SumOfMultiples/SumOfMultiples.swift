c22498cb9a4f21e1144672a10f29ce7e57804822
Sunny <cd_sunyang@163.com>
2018-12-08 16:46:09 +0800

    SumOfMultiples

--- sum-of-multiples/Sources/SumOfMultiples/SumOfMultiples.swift
+++ sum-of-multiples/Sources/SumOfMultiples/SumOfMultiples.swift
@@ -1,1 +1,24 @@
 //Solution goes in Sources
+import Foundation
+
+struct SumOfMultiples {
+    
+    static func toLimit(_ value: Int, inMultiples: [Int] = []) -> Int {
+        if value < inMultiples.min() ?? 0 {
+            return 0
+        }
+        
+        if value <= 1 {
+            return 0
+        }
+        return (1..<value).filter({ s -> Bool in
+            
+            for i in inMultiples.filter({$0 != 0}) {
+                if s % i == 0 {
+                    return true
+                }
+            }
+            return false
+        }).reduce(0, +)
+    }
+}
