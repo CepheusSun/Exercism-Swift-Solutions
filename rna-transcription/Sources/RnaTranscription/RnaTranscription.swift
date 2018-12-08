9aea302703d0a7436caab38b072739ccdab33502
Sunny <cd_sunyang@163.com>
2018-12-08 16:29:35 +0800

    udate

--- rna-transcription/Sources/RnaTranscription/RnaTranscription.swift
+++ rna-transcription/Sources/RnaTranscription/RnaTranscription.swift
@@ -1,5 +1,47 @@
 //Solution goes in Sources
+import Foundation
 
 struct Nucleotide {
     var dna: String
+    
+    init(_ dna: String) {
+        self.dna = dna
+    }
+    
+    func complementOfDNA() throws -> String {
+        
+        var rna = ""
+        for c in dna {
+            let res = try convert(c)
+            rna = rna + String(res)
+        }
+        return rna
+    }
+    
+    func convert(_ s: Character) throws -> Character {
+        switch s {
+        case "G": return "C"
+        case "T": return "A"
+        case "A": return "U"
+        case "C": return "G"
+        default: throw RnaTranscription.TranscriptionError.invalidNucleotide("\(s) is not a valid Nucleotide")
+        }
+    }
+    
+
+}
+
+struct RnaTranscription {
+    
+    enum TranscriptionError: Error, CustomStringConvertible {
+        
+        case invalidNucleotide(String)
+        
+        var description: String {
+            switch self {
+            case .invalidNucleotide(let c): return c
+            }
+        }
+        
+    }
 }
