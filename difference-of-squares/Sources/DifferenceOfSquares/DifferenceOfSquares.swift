4684e5d56f0fd918dd985dc66d285e306d680b98
Sunny <cd_sunyang@163.com>
2018-12-08 14:54:06 +0800

    solution 1

--- difference-of-squares/Sources/DifferenceOfSquares/DifferenceOfSquares.swift
+++ difference-of-squares/Sources/DifferenceOfSquares/DifferenceOfSquares.swift
@@ -3,4 +3,31 @@ import Foundation
 
 struct Squares {
     
+    private var list: [Int] = []
+    
+    init(_ value: Int) {
+        for i in 1...value {
+            self.list.append(i)
+        }
+    }
+    
+    var squareOfSum: Int {
+        let sum = list.sum
+        return sum * sum
+    }
+    
+    var sumOfSquares: Int {
+        return list.map { $0 * $0 }.sum
+    }
+    
+    var differenceOfSquares: Int {
+        return squareOfSum - sumOfSquares
+    }
+}
+
+extension Array where Element == Int {
+
+    var sum: Int {
+        return self.reduce(0, +)
+    }
 }
