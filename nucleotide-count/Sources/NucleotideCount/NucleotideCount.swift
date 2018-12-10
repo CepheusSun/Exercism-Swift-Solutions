d7d3a7feac3f9e91dcb6b04160e5060a3e48723e
Sunny <cd_sunyang@163.com>
2018-12-10 11:24:45 +0800

    A

--- nucleotide-count/Sources/NucleotideCount/NucleotideCount.swift
+++ nucleotide-count/Sources/NucleotideCount/NucleotideCount.swift
@@ -1,1 +1,32 @@
 //Solution goes in Sources
+import Foundation
+
+struct DNA {
+    
+    private let strand: String
+    
+    private var nucleotideCounts = ["A":0, "G":0, "C":0, "T":0]
+    
+    init?(strand: String) {
+        let regexString = "^[ATCG]*$"
+
+        let predicate = NSPredicate(format: "SELF MATCHES %@", regexString)
+        guard predicate.evaluate(with: strand) else { return nil }
+        self.strand = strand
+        
+        
+        self.strand.forEach {
+            self.nucleotideCounts[String($0)] = self.nucleotideCounts[String($0)]! + 1
+        }
+        
+    }
+    
+    func count(_ str: String) -> Int {
+        return nucleotideCounts[str] ?? 0
+    }
+    
+    func counts() -> [String: Int] {
+        return nucleotideCounts
+    }
+    
+}
