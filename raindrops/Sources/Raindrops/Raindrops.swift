49548da4658e468476a1d4b4483fcd0f99cf9ae8
Sunny <cd_sunyang@163.com>
2018-12-10 15:26:37 +0800

    A

--- raindrops/Sources/Raindrops/Raindrops.swift
+++ raindrops/Sources/Raindrops/Raindrops.swift
@@ -1,1 +1,28 @@
 //Solution goes in Sources
+import Foundation
+
+struct Raindrops {
+    
+    private let times: UInt
+    
+    init(_ times: UInt) {
+        self.times = times
+    }
+    
+    var sounds: String {
+        get {
+            var res = ""
+            if times % 3 == 0 {
+                res.append("Pling")
+            }
+            if times % 5 == 0 {
+                res.append("Plang")
+            }
+            if times % 7 == 0 {
+                res.append("Plong")
+            }
+            return res == "" ? "\(times)" : res
+        }
+    }
+
+}
