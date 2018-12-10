ca1809f83b9fcbda865504526ba262d4fa00ff1c
Sunny <cd_sunyang@163.com>
2018-12-10 10:52:46 +0800

    A

--- hamming/Sources/Hamming/Hamming.swift
+++ hamming/Sources/Hamming/Hamming.swift
@@ -1,1 +1,11 @@
 //Solution goes in Sources
+import Foundation
+
+struct Hamming {
+    
+    static func compute(_ input: String, against: String) -> Int? {
+        guard input.count == against.count else { return nil }
+
+        return zip(input, against).filter({$0 != $1}).count
+    }
+}
