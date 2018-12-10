0c48ca6be1e4fd362ca386247b9c761503001e15
Sunny <cd_sunyang@163.com>
2018-12-10 10:26:11 +0800

    update

--- grains/Sources/Grains/Grains.swift
+++ grains/Sources/Grains/Grains.swift
@@ -1,1 +1,26 @@
 //Solution goes in Sources
+import Foundation
+
+struct Grains {
+    
+    static var total = UInt64.max
+    
+    static func square(_ input: Int) throws -> UInt64 {
+        let errorMsg = "Input[\(input)] invalid. Input should be between 1 and 64 (inclusive)"
+        switch input {
+        case 1...64:
+            return UInt64(pow(2.0, Double(input - 1)))
+        case ..<1:
+            throw GrainsError.inputTooLow(errorMsg)
+        case 64...:
+            throw GrainsError.inputTooHigh(errorMsg)
+        default: return 0
+        }
+
+    }
+    
+    enum GrainsError: Error {
+        case inputTooLow(String)
+        case inputTooHigh(String)
+    }
+}
