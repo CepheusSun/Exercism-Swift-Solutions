783e89e4865cdb43f7a566d039313310626c8af6
Sunny <cd_sunyang@163.com>
2018-12-11 10:25:19 +0800

    update

--- grade-school/Sources/GradeSchool/GradeSchool.swift
+++ grade-school/Sources/GradeSchool/GradeSchool.swift
@@ -1,1 +1,28 @@
 //Solution goes in Sources
+import Foundation
+
+class GradeSchool {
+    
+    struct Student {
+        let name: String
+        let grade: Int
+    }
+    
+    private var list: [Student] = []
+    
+    func addStudent(_ name: String, grades: Int) {
+        list.append(Student(name: name, grade: grades))
+    }
+    
+    var roster: [Int: [String]] {
+        return [:]
+    }
+    
+    var sortedRoster: [[Int: [String]]] {
+        
+        list.sort(by: { return $0.grade > $1.grade} )
+        
+        return []
+    }
+}
+
