495dbb6bb7b07e2cd784a4a476366f11adf0109d
Sunny <cd_sunyang@163.com>
2018-12-22 13:10:31 +0800

    sloution for ELS

--- etl/Sources/Etl/Etl.swift
+++ etl/Sources/Etl/Etl.swift
@@ -1,1 +1,16 @@
 //Solution goes in Sources
+
+import Foundation
+
+struct ETL {
+    static func transform(_ old: [Int: [String]]) -> [String: Int] {
+        
+        var res: [String: Int] = [:]
+        for (key, value) in old {
+            value.forEach {
+                res[$0.lowercased()] = key
+            }
+        }
+        return res
+    }
+}
